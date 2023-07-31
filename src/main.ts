91a3890bfc9985fc93eeb91abe2483cf2252d47e
SpiritOfForest <dzahar@mail.ru>
2023-07-31 23:22:53 +0400

    feat: add the 'doc' route with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+
 import { AppModule } from './app.module';
+import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const data = readFileSync('./doc/api.yaml', 'utf8');
+  const yamlData = yaml.load(data);
+
+  SwaggerModule.setup('doc', app, yamlData);
+
+  await app.listen(config.PORT);
 }
 bootstrap();
