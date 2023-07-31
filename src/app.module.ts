1f08a20643f850d199d77eb97e2ecbeda5e548e1
SpiritOfForest <dzahar@mail.ru>
2023-07-31 21:38:36 +0400

    fix: missing favorites module dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [UsersModule, TracksModule, ArtistModule, AlbumModule],
+  imports: [UsersModule, TracksModule, ArtistModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
