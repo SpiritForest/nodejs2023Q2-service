16cbfe7215159027db88a074d60bd1723ccc313b
Dzmitry Zakhar <dzmitry_zakhar@epam.com>
2023-08-14 12:06:58 +0400

    eslint: format code

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -3,15 +3,15 @@ import { FileLogger } from '../utils/FileLogger';
 import { DataSource } from 'typeorm';
 
 const dataSource = new DataSource({
-  type: 'postgres',             
-  host: 'localhost',            
+  type: 'postgres',
+  host: 'localhost',
   port: config.POSTGRES_PORT,
   username: config.POSTGRES_USER,
-  password: config.POSTGRES_PASSWORD,   
-  synchronize: false,           
-  logging: true,   
-  logger: new FileLogger(true, '/logs'),             
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'], 
+  password: config.POSTGRES_PASSWORD,
+  synchronize: false,
+  logging: true,
+  logger: new FileLogger(true, '/logs'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*.ts'],
 });
 
