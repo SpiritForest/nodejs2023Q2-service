1edb1eec22e0c7a4cca6ade33418e24363698848
SpiritOfForest <dzahar@mail.ru>
2023-08-17 00:15:34 +0400

    feat: generate auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { TracksModule } from './tracks/tracks.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TracksModule, ArtistModule, AlbumModule, FavsModule],
+  imports: [UsersModule, TracksModule, ArtistModule, AlbumModule, FavsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
